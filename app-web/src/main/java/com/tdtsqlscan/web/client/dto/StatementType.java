8f954b8b181a8cd6662937726955a5c39c060edc
Azriel84ht <el.sirviente.de.los.huesos@gmail.com>
2025-09-23 17:40:31 +0200

    app-web eliminado

--- app-web/src/main/java/com/tdtsqlscan/web/client/dto/StatementType.java
+++
@@ -1,7 +0,0 @@
-package com.tdtsqlscan.web.client.dto;
-
-public enum StatementType {
-    BTEQ_COMMAND,
-    SQL_DML,
-    SQL_DDL
-}
--- app-web/src/main/java/com/tdtsqlscan/web/repository/SuggestionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tdtsqlscan.web.repository;
-
-import com.tdtsqlscan.web.domain.Suggestion;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SuggestionRepository extends JpaRepository<Suggestion, Long> {
-}
